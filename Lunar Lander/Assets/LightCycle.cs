e6b01986009c37ce0c7e3760c8ed0b7be68c4b73
Ganon <1013035@student.sae.edu.au>
2018-10-08 17:22:21 +0800

    Initial commit

---
+++ Lunar Lander/Assets/LightCycle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCycle : MonoBehaviour
+{
+
+    Transform transform;
+
+    Vector3 move = new Vector3(0, 1);
+
+    [SerializeField]
+    float speed;
+
+    Vector3 lastTurnPosition;
+   
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        lastTurnPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(move * speed);
+
+        Debug.DrawLine(transform.position, lastTurnPosition, Color.yellow, 20f);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Turn(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Turn(false);
+        }
+	}
+
+    void Turn(bool left)
+    {
+
+        if (move == Vector3.up)
+        {
+            move = (left ? Vector3.left : Vector3.right);
+        }
+        else if (move == Vector3.down)
+        {
+            move = (left ? Vector3.right : Vector3.left);
+        }
+        else if (move == Vector3.left)
+        {
+            move = (left ? Vector3.down : Vector3.up);
+        }
+        else if (move == Vector3.right)
+        {
+            move = (left ? Vector3.up : Vector3.down);
+        }
+
+        lastTurnPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        Destroy(gameObject);
+    }
+}
---
+++ Lunar Lander/Assets/ShipController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField]
+    Camera mainCamera;
+
+    Rigidbody rb2d;
+
+    Transform transform;
+
+    float input;
+
+    [SerializeField]
+    float rocketForce;
+
+    [SerializeField]
+    GameObject flame;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody>();
+
+        transform = GetComponent<Transform>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
+        input = Input.GetAxis("Horizontal");
+
+        rb2d.angularVelocity = new Vector3(0, 0, -input);
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("fire");
+            rb2d.AddForce(transform.up * rocketForce);
+            flame.SetActive(true);
+        }
+        else
+        {
+            flame.SetActive(false);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
