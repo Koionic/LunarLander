6c8d192f2426c0ba142620c33cb7df08fb5c7282
Ganon <1013035@student.sae.edu.au>
2018-10-16 13:01:33 +0800

    Update Lunar Lander/Assets/Scripts/InputController.cs

--- Lunar Lander/Assets/Scripts/InputController.cs
+++ Lunar Lander/Assets/Scripts/InputController.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class InputController : MonoBehaviour 
 {
 
-    string throttleInputKey;
-
-    bool mac;
+    string throttleInputKey, submitKey, cancelKey;
 
     void Start()
     {
         if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX)
         {
             throttleInputKey = "ThrottleMac";
-            mac = true;
+            submitKey = "SubmitMac";
+            cancelKey = "CancelMac";
         }
         else
         {
             throttleInputKey = "Throttle";
-            mac = false;
+            submitKey = "Submit";
+            cancelKey = "Cancel";
         }
     }
 
@@ -45,6 +45,7 @@ public class InputController : MonoBehaviour
     /// <param name="id">Player ID.</param>
     public float GetThrottleInput(int id)
     {
+        Debug.Log("Grabbing " + throttleInputKey + id);
         return Input.GetAxis(throttleInputKey + id);
     }
 
@@ -55,29 +56,7 @@ public class InputController : MonoBehaviour
     /// <param name="id">Identifier.</param>
     public bool SelectIsPressed(int id)
     {
-        switch (id)
-        {
-            case 1:
-                {
-                    return Input.GetKey(mac ? KeyCode.Joystick1Button16 : KeyCode.Joystick1Button0);
-                }
-            case 2:
-                {
-                    return Input.GetKey(mac ? KeyCode.Joystick2Button16 : KeyCode.Joystick2Button0);
-                }
-            case 3:
-                {
-                    return Input.GetKey(mac ? KeyCode.Joystick3Button16 : KeyCode.Joystick3Button0);
-                }
-            case 4:
-                {
-                    return Input.GetKey(mac ? KeyCode.Joystick4Button16 : KeyCode.Joystick4Button0);
-                }
-            default:
-                {
-                    return Input.GetKey(mac ? KeyCode.JoystickButton16 : KeyCode.JoystickButton0);
-                }
-        }
+        return Input.GetButtonDown(submitKey + id);
     }
 
     /// <summary>
@@ -87,28 +66,6 @@ public class InputController : MonoBehaviour
     /// <param name="id">Identifier.</param>
     public bool CancelIsPressed(int id)
     {
-        switch (id)
-        {
-            case 1:
-                {
-                    return Input.GetKeyDown(mac ? KeyCode.Joystick1Button17 : KeyCode.Joystick1Button1);
-                }
-            case 2:
-                {
-                    return Input.GetKeyDown(mac ? KeyCode.Joystick2Button17 : KeyCode.Joystick2Button1);
-                }
-            case 3:
-                {
-                    return Input.GetKeyDown(mac ? KeyCode.Joystick3Button17 : KeyCode.Joystick3Button1);
-                }
-            case 4:
-                {
-                    return Input.GetKeyDown(mac ? KeyCode.Joystick4Button17 : KeyCode.Joystick4Button1);
-                }
-            default:
-                {
-                    return Input.GetKeyDown(mac ? KeyCode.JoystickButton17 : KeyCode.JoystickButton1);
-                }
-        }
+        return Input.GetButtonDown(cancelKey + id);
     }
 }
