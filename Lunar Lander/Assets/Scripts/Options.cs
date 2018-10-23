76bc502675c58823ade37ce98ead9da67be8952b
MechaWilliam <40289865+MechaWilliam@users.noreply.github.com>
2018-10-23 19:14:16 +0800

    options menu

---
+++ Lunar Lander/Assets/Scripts/Options.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    private AudioManager audioManager;
+
+    [SerializeField]
+    private Toggle muteToggle;
+
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+
+        if (audioManager.isMuted)
+        {
+            muteToggle.isOn = true;
+        }
+
+    }
+
+    // links options slider to audiomixer groups
+    public void SetVolumeMaster(float volume)
+    {
+        audioMixer.SetFloat("Master", volume);
+    }
+    public void SetVolumeMusic(float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+    public void SetVolumeSFX(float volume)
+    {
+        audioMixer.SetFloat("SFX", volume);
+    }
+
+    // pauses audio listeners to mute all audio
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        audioManager.isMuted = !audioManager.isMuted;
+    }
+}
